9e286c472e7951099822a1ece5c5ff02b6072d35
najmi10 <53500211+najmi10@users.noreply.github.com>
2020-04-23 09:25:07 +0700

    Add files via upload

---
+++ corona_web/templateica/index.php
@@ -0,0 +1,115 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS Bootstraps -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    
+    <!-- CSS Manual -->
+    <link rel="stylesheet" href="style.css" type="text/css">
+    
+</head>
+<body>
+    
+    <!-- Navbar -->
+    <section id="mynav">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+        <div class="container">
+        <a class="navbar-brand ml-4" href="#">COVID-19</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mr-4" href="#">Test</a>
+            </li>
+            <li>
+            <a href=""><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button></a>  
+            </li>
+            </ul>
+            </form>
+        </div>
+        </div>
+        </nav> 
+        </section>
+        <!-- end navbar -->
+
+    <!-- Section Header -->
+    <section id="header">
+        <div class="container">
+        <div class="row">
+            
+            <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                <h1 data-aos="fade-right">iki judule</h1>
+                <h2 data-aos="fade-right" data-aos-delay="400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur iste, iusto amet numquam eligendi natus architecto tempora incidunt sapiente. Nemo, impedit commodi. Recusandae saepe fugiat ipsam voluptatum dolorum explicabo doloremque.</p>
+                </h2>
+                <div data-aos="fade-left" data-aos-delay="800">
+                    <a href="#about" class="btn-get-started scrollto ">CHECK UP</a>
+                </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+                <img src="img/covid2.png" class="img-fluid animated" alt="">
+
+            </div>
+        </div>
+    </div>
+    </section>
+    <!-- end header -->
+
+    <!-- section about -->
+    <section id="about">
+        <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-6 order-1 order-lg-1 hero-img" data-aos="fade-left" data-aos-delay="200">
+                <img src="img/" alt="" class="img-fluid animated"> <!-- fotomu kekno kene -->
+            </div>
+
+            <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-2 d-flex flex-column justify-content-center">
+                <h3>About Me</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente omnis natus vitae perferendis labore voluptas reprehenderit sint eum deleniti ratione blanditiis, accusamus perspiciatis minima, exercitationem neque quasi! Perspiciatis, fuga tempore.</p>
+            </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- end About -->
+
+    <!-- Section footer  -->
+    <section id="footer">
+        <div class="footer">
+        </div>
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-6 order-1 order-lg-1 hero-img">
+                <h3>Tentang Kami</h3>
+                <ul class="tentang">
+                    <li><a href="">instagram</a></li>
+                    <li><a href="">twitter</a></li>
+                    <li><a href="">facebook</a></li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+
+
+</body>
+</html>
